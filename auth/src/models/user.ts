587b6880a54eec89d196a461c29bd364565f79c7
abir <amabirbd@gmail.com>
2026-01-27 22:27:10 +0600

    Add mongoose integration and user signup functionality
    - Updated package.json and package-lock.json to include mongoose as a dependency.
    - Refactored index.ts to establish a connection to MongoDB using mongoose.
    - Enhanced signup route to check for existing users and handle user creation with appropriate error handling.

---
+++ auth/src/models/user.ts
@@ -0,0 +1,49 @@
+import mongoose, { model, Schema } from "mongoose";
+import { Password } from "../services/password.js";
+
+interface UserAttrs {
+    email: string,
+    password: string
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+    email: string,
+    password: string
+}
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+userSchema.pre('save', async function(done) {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed)
+    }
+    // done();
+})
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs)
+}
+
+const User = model<UserDoc, UserModel>('User', userSchema);
+
+User.build({
+    email: "afsdf",
+    password: "asdfasf"
+})
+
+
+export { User }
